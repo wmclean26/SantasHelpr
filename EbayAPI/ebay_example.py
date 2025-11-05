e3f84c3467e56455ad2746e6ddef2d0d4a76c605
Jonah D <daltonj5@mailbox.winthrop.edu>
2025-11-04 22:19:01 -0500

    Added the ebay API with JSON and Modularity Changes

---
+++ EbayAPI/ebay_example.py
@@ -0,0 +1,20 @@
+from ebay_call import run_search  # Correct import statement
+
+def main():
+    """
+    Demonstrates how to use the run_search function.
+    """
+
+    print("\n[CALL: Searching for 'Lego Star wars' with Filters]")
+    output_json_filtered = run_search(
+        query="Lego star wars",
+        min_price=30,
+        max_price=100,
+        condition="USED, NEW",
+        output_file="search1.json"
+    )
+    print(output_json_filtered)
+
+
+if __name__ == "__main__":
+    main()
