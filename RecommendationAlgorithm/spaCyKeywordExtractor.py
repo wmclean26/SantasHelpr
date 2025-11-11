7748e6c4eb85126faa7fe0468f1a3f4adcd07b60
wmclean26 <wmclean@citadel.edu>
2025-11-10 21:00:21 -0500

    spacy+regex

--- RecommendationAlgorithm/spaCyKeywordExtractor.py
+++ RecommendationAlgorithm/spaCyKeywordExtractor.py
@@ -1,13 +1,29 @@
 import spacy
+import re
 
-# Load pre-trained model
+
 nlp = spacy.load('en_core_web_sm')
 
-# Process user query
-text = "I need to find a gift for my 10 year old niece under 15 dollars"
-doc = nlp(text)
 
-# Extract named entities
-for ent in doc.ents:
-    print(f"{ent.text}: {ent.label_}")
-    # Output: "10": CARDINAL, "15 dollars": MONEY
+def extract_entities(query):
+    doc = nlp(query)
+    entities = {}
+
+    for ent in doc.ents:
+        if ent.label_ == 'MONEY':
+            # Extract numeric value from money entity
+            amount = re.search(r'(\d+(?:\.\d{2})?)', ent.text)
+            if amount:
+                entities['budget'] = float(amount.group(1))
+        elif ent.label_ == 'DATE':
+            entities['occasion_date'] = ent.text
+        elif ent.label_ == 'CARDINAL':
+            # Might be age
+            entities['age_candidate'] = int(ent.text)
+
+    return entities
+
+
+query = "I need to find a gift for my 10 year old niece under 15 dollars"
+entities = extract_entities(query)
+print(entities)
